2dc18728c1837f8958324cd6a4923487b6128b5e
SitanshuChauhan <83861158+SitanshuChauhan@users.noreply.github.com>
2022-04-09 09:18:37 +0530

    Add files via upload

---
+++ Java/Valid Username Regular Expression .java
@@ -0,0 +1,7 @@
+//https://www.hackerrank.com/challenges/valid-username-checker/problem?isFullScreen=true
+
+
+class UsernameValidator {
+    public static final String regularExpression = "^[a-zA-Z][\\w]{7,29}$";
+}
+
